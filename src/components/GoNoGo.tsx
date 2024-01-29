35d73ec722022026236666b91c2a97d904614a28
Henry <heinrichk91@gmail.com>
2024-01-29 17:46:00 +0100

    working

--- src/components/GoNoGo.tsx
+++ src/components/GoNoGo.tsx
@@ -7,132 +7,138 @@ function GoNoGo({ openMenu }: { openMenu: () => void }) {
   const [activeRound, setActiveRound] = useState<boolean>(false);
   const [rerunRound, setRerunRound] = useState<boolean>(false);
   const [goRound, setGoRound] = useState<number>(0);
-  const [goNoRound, setNoGoRound] = useState<number>(0);
+  const [goNoRound, setGoNoRound] = useState<number>(0);
   const [goScore, setGoScore] = useState<number>(0);
   const [noGoScore, setNoGoScore] = useState<number>(0);
-  const [span, setSpan] = useState<number>(5);
+  const [span, setSpan] = useState<number>(2);
   const [checkForward, setCheckForward] = useState<boolean>(true);
   const [isSlow, setIsSlow] = useState<boolean>(true);
   const [calc, setCalc] = useState<string>("");
   const [message, setMessage] = useState<string>("");
   const [retVal, setRetVal] = useState<string>("0/0");
-  const [guessList, setGuessList] = useState<string[]>([]);
-  const [intervalIdState, setIntervalIdState] = useState<string>();
   const [timeoutId, setTimeoutId] = useState<NodeJS.Timeout>();
+  const [shouldCheckConditions, setShouldCheckConditions] = useState<boolean>(false);
 
-  const addGoRound = (correct: boolean) => {
-    setGoRound(goRound + 1);
-    setGoScore(goScore + (correct ? 1 : 0));
-  };
-  const addNoGoRound = (correct: boolean) => {
-    setNoGoRound(goNoRound + 1);
-    setNoGoScore(noGoScore + (correct ? 1 : 0));
-  };
+  const addGoRound = useCallback(
+    (correct: boolean) => {
+      console.log("");
+      const score = goScore + (correct ? 1 : 0);
+      const round = goRound + 1;
+      console.log("addGoRound", score, round);
+      console.log("");
+      setGoRound(round);
+      setGoScore(score);
+      const msg = `Go: ${score}/${round} NoGO: ${noGoScore}/${goNoRound}`;
+      console.log("msg", msg);
+      setRetVal(msg);
+    },
+    [goRound, goScore, goNoRound, noGoScore]
+  );
+  const addNoGoRound = useCallback(
+    (correct: boolean) => {
+      const score = noGoScore + (correct ? 1 : 0);
+      const round = goNoRound + 1;
+      console.log("addGoRound", score, round);
+      setGoNoRound(round);
+      setNoGoScore(score);
+      const msg = `Go: ${goScore}/${goRound} NoGO: ${score}/${round}`;
+      setRetVal(msg);
+    },
+    [goNoRound, goRound, goScore, noGoScore]
+  );
 
-  const delaydRerun = (delay: number) => {
-    setCalc("");
-    console.log("delaydRerun activeRound", activeRound);
-    setTimeout(() => {
-      setMessage("");
-      runTest();
-      // restart();
-    }, delay);
-  };
-
-  const checkRunOutConditions = (calc: string) => {
-    console.log("calc", calc);
-    if (calc === "1") {
-      setMessage("Correct !!!");
-      addNoGoRound(true);
-      delaydRerun(1000);
-    } else {
-      setMessage("You should have pressed it");
-      console.log("activeRound", activeRound);
-      addGoRound(false);
-      delaydRerun(2000);
-    }
-  };
   const runTest = useCallback(() => {
     console.log("activeRound", activeRound);
-    let intervalId: any = null;
 
     const setOption = () => {
       const rand = Math.random();
       console.log("rand", rand);
-      const button = rand > 0.2 ? "2" : "1";
-      // const button = rand < 0.8 ? "2" : "1";
+      const button = rand > span / 10 ? "2" : "1";
       console.log("button", button);
       setCalc(button);
       const timeoutId = setTimeout(() => {
-        checkRunOutConditions(button);
+        setShouldCheckConditions(!shouldCheckConditions);
       }, 2000);
       setTimeoutId(timeoutId);
     };
 
     setOption();
+  }, [activeRound, setShouldCheckConditions, shouldCheckConditions, span]);
 
-    setIntervalIdState(intervalId);
-  }, [calc, isSlow, activeRound]);
+  useEffect(() => {
+    if (shouldCheckConditions) {
+      setShouldCheckConditions(false);
+      if (calc === "1") {
+        setMessage("Correct !!!");
+        setCalc("");
+        setTimeout(() => {
+          setMessage("");
+          if (activeRound) setRerunRound(true);
+        }, 1000);
+        addNoGoRound(true);
+      } else {
+        setMessage("You should have pressed it");
+        setCalc("");
+        console.log("useEffect activeRound", activeRound);
+        addGoRound(false);
+        setTimeout(() => {
+          setMessage("");
+          if (activeRound) setRerunRound(true);
+        }, 1000);
+      }
+    }
+  }, [shouldCheckConditions, calc, activeRound, addGoRound, addNoGoRound]);
 
-  const startTest = useCallback(() => {
-    setRerunRound(false);
-    setCalc("");
-    setMessage("");
-    runTest();
-  }, [intervalIdState, runTest, span, activeRound]);
+  useEffect(() => {
+    if (rerunRound) {
+      setRerunRound(false);
+
+      runTest();
+    }
+  }, [rerunRound, setRerunRound, runTest]);
 
   const showCalc = ({ showCalc, retVal }: { showCalc: string; retVal: string }) => {
     console.log("showCalc", showCalc);
-    // if (!activeRound) return;
+
     clearTimeout(timeoutId);
     setTimeoutId(undefined);
 
-    console.log("calc", calc);
     if (calc === "1") {
       setMessage("You should not have pressed");
-      // addGoRound(false)
       addNoGoRound(false);
       setCalc("");
-      // setCalc("");
       setTimeout(() => {
         setMessage("");
-        if (activeRound) runTest();
-        //   // setRerunRound(true);
-        //   runTest([]);
-      }, 500);
-      // setActiveRound(false);
+        if (activeRound) setRerunRound(true);
+      }, 1000);
     } else {
-      console.log("ewqlkdnfoqwindpo");
       addGoRound(true);
       setMessage("Correct !!!");
-      // delaydRerun(200);
       setCalc("");
       setMessage("");
       setTimeout(() => {
         setMessage("");
-        if (activeRound) runTest();
-
-        // restart();
-      }, 500);
+        if (activeRound) setRerunRound(true);
+      }, 1000);
     }
   };
 
   const resetClick = () => {
     setCalc("");
-    setMessage("");
-    setGuessList([]);
+    setGoRound(0);
+    setGoScore(0);
+    setGoNoRound(0);
+    setNoGoScore(0);
+    setRetVal("0/0");
+    setActiveRound(false);
   };
 
   const restart = () => {
     console.log("activeRound", activeRound);
     if (!activeRound) {
       setRerunRound(true);
-      resetClick();
-      startTest();
     }
     setActiveRound(!activeRound);
-
-    // console.log("activeRound", activeRound);
   };
 
   const increaseSpan = () => {
